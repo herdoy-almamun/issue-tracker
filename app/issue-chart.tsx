4db8b090994b80cbfb32407d9e7b9742339fd00c
= <=>
2024-10-21 10:30:25 +0600

    Build The Issue Chart

---
+++ app/issue-chart.tsx
@@ -0,0 +1,49 @@
+"use client";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Rectangle,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueChart = ({ open, inProgress, closed }: Props) => {
+  const data = [
+    {
+      name: "Open",
+      value: open,
+    },
+    {
+      name: "In Progress",
+      value: inProgress,
+    },
+    {
+      name: "Closed",
+      value: closed,
+    },
+  ];
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart width={500} height={300} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Bar
+          dataKey="value"
+          className="bg-[--accent-9]"
+          activeBar={<Rectangle stroke="blue" />}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default IssueChart;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,35 @@
-import { Button } from "@radix-ui/themes";
+import IssueTable from "@/components/issue-table";
+import prisma from "@/prisma/client";
+import { Container, Grid } from "@radix-ui/themes";
+import { Suspense } from "react";
+import IssueChart from "./issue-chart";
 import Navbar from "./navbar";
 
-const Home = () => {
+const Home = async () => {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  const issues = await prisma.issue.findMany({
+    orderBy: { createdAt: "desc" },
+    take: 5,
+  });
   return (
-    <main>
+    <Suspense>
       <Navbar />
-      <Button>Button</Button>
-    </main>
+      <Container className="px-4">
+        <Grid columns={{ initial: "1", md: "2" }} gap="3">
+          <div className="h-[340px]">
+            <IssueChart open={open} inProgress={inProgress} closed={closed} />
+          </div>
+          <div>
+            <h1 className="text-2xl font-semibold mb-2">Latest Issues</h1>
+            <IssueTable issues={issues} />
+          </div>
+        </Grid>
+      </Container>
+    </Suspense>
   );
 };
 
